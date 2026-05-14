faea3f755060492ebac9b721e38c0c8eba29d069
Your Name <you@example.com>
2026-05-14 18:38:34 +0200

    add next_file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 print("Hallo")
 
 # git branch - список веток
-# git branch new_feature - создать ветку
+# git branch new_feature - создать ветку  new_feature-название версии
 # git checkout new_feature - переключиться
 # git push
 # git checkout -b new_feature - создать и переключиться сразу
-# git merge new_feature - слить ветку в текущую
+# git merge new_feature - слить ветку в текущую (находимся на какой-то ветке и сливаем все в указаннную ветку new_feature)
 # git branch -d new_feature - удалить ветку
