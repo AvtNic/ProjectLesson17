31bc0a76e7e8e5c66fa86209b555c4915b6881af
Your Name <you@example.com>
2026-05-14 18:21:00 +0200

    new com

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Hallo")
+
+# git branch - список веток
+# git branch new_feature - создать ветку
+# git checkout new_feature - переключиться
+# git push
+# git checkout -b new_feature - создать и переключиться сразу
+# git merge new_feature - слить ветку в текущую
+# git branch -d new_feature - удалить ветку
